5a4cb1e7f7d58506b5bb0ca9215ad88e89111275
Naveen1854 <kusuma.naveen1854@gmail.com>
2022-10-04 17:29:33 +0530

    New File

---
+++ Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter First Number\n");
+	scanf("%d",&a);
+	printf("Enter Second Number\n");
+	scanf("%d",&b);
+	c = a+b;
+	printf("%d + %d = %d",a,b,c);
+	return 0;
+	}
+
